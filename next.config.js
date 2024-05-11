080828a8d5007c83a7e6034977314a0a4c2a766e
balashqedirli <balasqdirov4@gmail.com>
2024-05-11 18:13:38 +0400

    project

--- next.config.js
+++ next.config.js
@@ -1,3 +1,44 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   i18n: {
+//     locales: ['en', 'az', 'ru'],
+//     defaultLocale: 'en'
+//   },
+//   swcMinify: true,
+//   images: {
+//     domains: ['firebasestorage.googleapis.com'],
+//   },
+//   async headers() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         headers: [
+//           { key: 'Access-Control-Allow-Credentials', value: 'true' },
+//           { key: 'Access-Control-Allow-Origin', value: '*' },
+//           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+//           { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -22,6 +63,16 @@ const nextConfig = {
       },
     ];
   },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+     
+    };
+  },
 };
 
 module.exports = nextConfig;
+
